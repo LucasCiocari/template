c05dde039ed82a742c387eaf3c6260bba6a88268
Lucas Bertoglio Ciocari <lucasciocari@gmail.com>
2019-06-17 13:05:38 -0300

    updated fonts import

--- src/index.js
+++ src/index.js
@@ -1,28 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import GoogleFontLoader from 'react-google-font-loader'; 
-
-//Images
-
-
 import App from "./App";
 
 import "./index.scss";
-
-const fontNames = 
-    <GoogleFontLoader fonts={[
-            {
-                font: 'Roboto',
-                weights: [400]
-            },
-            {
-                font: 'Roboto Slab',
-                weights: [400]
-            }
-        ]
-    }/>
-
-
 
 ReactDOM.render(<App />, document.getElementById("root"));
